b13abdc1f2566b5c9499d3e235a876705c15f3a3
Suraj Bhari <surajbhari159@gmail.com>
2025-02-26 06:01:57 +0530

    i hate timezones

--- classes/Comment.py
+++ classes/Comment.py
@@ -1,5 +1,5 @@
-from datetime import datetime
-
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
 class Comment:
     def __init__(self, query):
         self.id = query[0]
@@ -7,7 +7,7 @@ class Comment:
         self.lead_id = query[2]
         self.comment = query[3]
         # 2025-02-25 22:42:06
-        self.created_at = datetime.strptime(query[4], "%Y-%m-%d %H:%M:%S")
+        self.created_at = datetime.strptime(query[4], "%Y-%m-%d %H:%M:%S").replace(tzinfo=ZoneInfo('Asia/Kolkata')) + timedelta(hours=5, minutes=30) # UTC+5:30
         self.time = self.created_at
     
     def json(self):
--- classes/FollowUp.py
+++ classes/FollowUp.py
@@ -1,15 +1,16 @@
 
-from datetime import datetime
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
 
 class FollowUp:
     def __init__(self, query):
         self.id = query[0]
         self.user_id = query[1]
         self.lead_id = query[2]
-        self.follow_up_time = datetime.strptime(query[3], "%Y-%m-%d %H:%M:%S")
+        self.follow_up_time = datetime.strptime(query[3], "%Y-%m-%d %H:%M:%S").replace(tzinfo=ZoneInfo('Asia/Kolkata')) + timedelta(hours=5, minutes=30) # UTC+5:30
         self.follow_up_user_id = query[4]
         self.remarks = query[5]
-        self.created_at = datetime.strptime(query[6], "%Y-%m-%d %H:%M:%S")
+        self.created_at = datetime.strptime(query[6], "%Y-%m-%d %H:%M:%S" ).replace(tzinfo=ZoneInfo('Asia/Kolkata')) + timedelta(hours=5, minutes=30) # UTC+5:30
         self.time = self.created_at
         
     def json(self):
