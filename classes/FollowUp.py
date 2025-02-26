3e600aeaa18cd1b77718d0bde4c00ebfe73d3c8d
Suraj Bhari <surajbhari159@gmail.com>
2025-02-26 06:09:02 +0530

    no follow up time

--- classes/FollowUp.py
+++ classes/FollowUp.py
@@ -7,7 +7,10 @@ class FollowUp:
         self.id = query[0]
         self.user_id = query[1]
         self.lead_id = query[2]
-        self.follow_up_time = datetime.strptime(query[3], "%Y-%m-%d %H:%M:%S").replace(tzinfo=ZoneInfo('Asia/Kolkata')) + timedelta(hours=5, minutes=30) # UTC+5:30
+        if query[3] is None:
+            self.follow_up_time = None
+        else:
+            self.follow_up_time = datetime.strptime(query[3], "%Y-%m-%d %H:%M:%S").replace(tzinfo=ZoneInfo('Asia/Kolkata')) + timedelta(hours=5, minutes=30) # UTC+5:30
         self.follow_up_user_id = query[4]
         self.remarks = query[5]
         self.created_at = datetime.strptime(query[6], "%Y-%m-%d %H:%M:%S" ).replace(tzinfo=ZoneInfo('Asia/Kolkata')) + timedelta(hours=5, minutes=30) # UTC+5:30
