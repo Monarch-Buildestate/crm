9ffc14cdbd1833f3bd3207c7bbdaa7f447605b04
SurajBhari <surajbhari159@gmail.com>
2025-09-26 00:09:57 +0530

    add test print

--- main.py
+++ main.py
@@ -594,6 +594,7 @@ def user_or_lead(number, users:typing.List[User], leads:typing.List[Lead]) -> ty
 
 @app.route("/facebook/lead/add", methods=["POST"])
 def add_facebook_lead():
+    print("Facebook lead received", request.args)
     name = request.args.get("FULL_NAME")
     phone_number = request.args.get("PHONE")
     phone_number = phone_number.replace("+", "")
